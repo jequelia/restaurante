ec8067fa45c0859eb89340cbb18b00ac6a2b5011
brunogamacatao@gmail.com <brunogamacatao@gmail.com>
2020-03-09 17:19:21 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BancoDeDados from './db/BancoDeDados';
 
 function App() {
+  const [pedidos, setPedidos] = useState([]);
+
+  const pedir = (produto) => {
+    setPedidos([...pedidos, produto]);
+  };
+
+  const calculaTotal = () => {
+    let total = 0.0;
+    pedidos.forEach(p => total += p.valor);
+    return total;
+  };
+
+  const renderProduto = (produto) => {
+    return (
+      <tr>
+        <td><img src={produto.foto}/></td>
+        <td>{produto.nome}</td>
+        <td>{produto.tipo}</td>
+        <td>R$ {produto.valor.toFixed(2)}</td>
+        <td><a href="#" class="myButton" onClick={() => pedir(produto)}>Pedir</a></td>
+      </tr>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table-cardapio">
+      <div className="header">Cardápio - 💰 R$ {calculaTotal().toFixed(2)}</div>
+      <table cellspacing="0">
+        <thead>
+          <tr>
+            <th>Foto</th>
+            <th>Nome</th>
+            <th>Tipo</th>
+            <th>Preço</th>
+            <th>Pedir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {BancoDeDados.getProdutos().map(renderProduto)}
+        </tbody>
+      </table>
     </div>
   );
 }
